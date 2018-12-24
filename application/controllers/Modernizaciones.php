db5dde3acf000f96b3a9272979770bc74a086444
Admin <jfchaparro33@misena.edu.co>
2018-12-24 10:00:28 -0500

    modernizaciones agrupacion por ot

--- application/controllers/Modernizaciones.php
+++ application/controllers/Modernizaciones.php
@@ -16,18 +16,23 @@ class Modernizaciones extends CI_Controller {
         $this->load->view('modernizaciones');
     }
 
-    // Tabla que muestran todos los tickets que se encuentran en estado documentador
+    // Tabla que muestran las ordenes que se encuentran en la tabla de modernizaciones
     public function c_get_modernizaciones() {
         $get_modernizaciones = $this->Dao_modernizaciones_model->get_modernizaciones();
         echo json_encode($get_modernizaciones);
     }
 
+    //trae todas las modernizaciones que tengan el mismo id orden
     public function c_getOrdenDetail($idOrden) {
-
-        
+        $datos['idOrden'] = $idOrden;
         $this->load->view('ordenModerDetail', $datos);
     }
 
+    public function c_getOrdenDetailsModerById() {
+        $idOrden = $this->input->post('idOrden');
+        $get_modernizaciones = $this->Dao_modernizaciones_model->getOrdenDetailsModerById($idOrden);
+        echo json_encode($get_modernizaciones);
+    }
 }
 
 ?>
