d5afcce5a1e8f9e0014b99ce67f4e91d327d4fa9
bredybuitrago <bredybuitrago@gmail.com>
2019-01-11 14:57:17 -0500

    quitar col span

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -75,7 +75,7 @@
 		      		<td>F CREACION</td>
 		      		<td>SOLICITANTE</td>
               <td>REGION</td>
-		      		<td colspan="10">DESCRIPCION</td>
+		      		<td>DESCRIPCION</td>
 
 				</tr>
 <?php 
@@ -114,7 +114,7 @@
 				      		echo '<td>'.$value->F_CREACION.'</td>';
 				      		echo '<td>'.$value->SOLICITANTE.'</td>';
                   echo '<td>'.$value->REGION.'</td>';
-				      		echo '<td colspan="10">'.$value->DESCRIPCION.'</td>';
+				      		echo '<td>'.$value->DESCRIPCION.'</td>';
 						echo '</tr>';
 					}
 
