949447386082239db127ade58590be2390d11514
Leonel Oliveros <leoneloliveros.co@gmail.com>
2019-06-28 09:43:48 -0500

    cambios de ingenieros de T20

--- application/config/db.php
+++ application/config/db.php
@@ -24,7 +24,7 @@ return [
       |
      */
 
-    'default' => 'local',
+    'default' => 'servidor',
     /*
       |--------------------------------------------------------------------------
       | Database Connections
